8b8e632779a5f714dc8e54b5bf5f8ddecfc3a354
Graham Bing <grrraham@gmail.com>
2020-05-04 00:50:15 -0700

    Stub Data class

---
+++ Jackdaw/Data.swift
@@ -0,0 +1,23 @@
+//
+//  Data.swift
+//  Jackdaw
+//
+//  Created by Graham Bing on 2020-05-04.
+//  Copyright © 2020 Corvus Corax. All rights reserved.
+//
+
+import Foundation
+
+func newNote() -> Note {
+    
+}
+
+func save() {
+    
+}
+
+func saveUIImage(image: UIImage) -> String {
+    // return the reference name
+    // https://fluffy.es/store-image-coredata/
+    return ""
+}
