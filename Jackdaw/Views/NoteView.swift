c6102a4b8a30e466a0d966a52a853055378ec531
Graham Bing <grrraham@gmail.com>
2020-05-14 19:38:59 -0700

    NoteView textContainerInset avoids keyboard

--- Jackdaw/Views/NoteView.swift
+++ Jackdaw/Views/NoteView.swift
@@ -15,7 +15,8 @@ struct NoteView: View {
     @ObservedObject var note: Note
     @State private var showPhotoPicker: Bool = false
     @State private var photoPickerImage: UIImage?
-    
+    @ObservedObject var keyboardResponder = KeyboardResponder()
+
     var body: some View {
 //        ScrollView {
 //            GeometryReader { proxy in
@@ -28,8 +29,19 @@ struct NoteView: View {
 //                                .frame(width:proxy.size.width)
 //                        }.buttonStyle(PlainButtonStyle())
 //                    }
-                    NoteTextFieldView(note: self.note)
-                        .padding()
+        VStack {
+            GeometryReader { proxy in
+                NoteTextFieldView(note: self.note)
+                    .padding(.horizontal)
+                    .padding(.top)
+//                    .frame(maxHeight: proxy.size.height - max(self.keyboardResponder.currentHeight - (UIScreen.main.bounds.size.height-proxy.frame(in: .global).maxY), 0))
+
+//                    .frame(maxHeight: proxy.size.height-self.keyboardResponder.currentHeight)
+                
+                Spacer()
+            }
+        }
+        
 //                }
 //                .sheet(isPresented: self.$showPhotoPicker,
 //                       onDismiss: self.updateNoteWithImage) {
@@ -57,15 +69,19 @@ struct NoteView: View {
 struct NoteTextFieldView: UIViewRepresentable {
     @Environment(\.managedObjectContext) var managedObjectContext
     @ObservedObject var note: Note
+    @ObservedObject var keyboardResponder = KeyboardResponder()
+    @State var view: UITextView = UITextView()
 
     func makeUIView(context: UIViewRepresentableContext<NoteTextFieldView>) -> UITextView{
-        let view = UITextView()
+//        view = UITextView()
         let text = self.note.text
+        
         view.attributedText = Typography.attributedStringFrom(string: text)
         view.isEditable = true
         view.backgroundColor = .clear
         view.delegate = context.coordinator
         view.keyboardDismissMode = .interactive
+
         return view
     }
     
@@ -83,17 +99,19 @@ struct NoteTextFieldView: UIViewRepresentable {
         
         init(parent: NoteTextFieldView) {
             self.parent = parent
+            super.init()
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardHideShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil);
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardHideShow(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil);
         }
-        
+                
         func textViewDidBeginEditing(_ textView: UITextView) {
         }
         
         func textViewDidEndEditing(_ textView: UITextView) {
-            if textView.text == "" {
-                print("textViewDidEndEditing deleted empty note")
+            if textView.text == "" && self.parent.note.image == nil {
                 UserData().delete(note: self.parent.note)
             } else {
-                print("textViewDidEndEditing saved: \(String(describing: textView.text))")
                 UserData().save()
             }
         }
@@ -108,34 +126,27 @@ struct NoteTextFieldView: UIViewRepresentable {
             savedRecently = true
             Timer.scheduledTimer(withTimeInterval: 1, repeats: false) {_ in
                 UserData().save()
-                print("textViewDidChange saved: \(textView.text!)\n\(self.parent.note.id!.uuidString)")
                 savedRecently = false
-            }
+            } 
+        }
+        @objc func keyboardHideShow(sender: NSNotification) {
+            self.parent.view.textContainerInset = UIEdgeInsets(top: 0, left: 8, bottom: self.parent.keyboardResponder.currentHeight, right: 8)
+        }
+
+        @objc func keyboardWillHide(sender: NSNotification) {
+            self.parent.view.textContainerInset = UIEdgeInsets(top: 0, left: 8, bottom: self.parent.keyboardResponder.currentHeight + 40, right: 8)
         }
     }
 }
 
-// MARK - Preview
-
-struct TextFieldPreviewView: UIViewRepresentable {
-    var text: String
-    
-    func makeUIView(context: UIViewRepresentableContext<TextFieldPreviewView>) -> UITextView {
-        let view = UITextView()
-        view.attributedText = Typography.attributedStringFrom(string: text)
-        view.isEditable = true
-
-        return view
-    }
-    
-    func updateUIView(_ uiView: UITextView, context: UIViewRepresentableContext<TextFieldPreviewView>) {
-        
-    }
-}
+// MARK: -
 
 struct NoteDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        TextFieldPreviewView(text: "Jackdaw\nA fine grey and black bird with pale eyes, related to but smaller than the carrion crow.\nThough thievish and mischievous to the farmer, the jackdaw is a curious and intelligent bird. It has been observed using tools for complex problem solving, like its Corvid cousins.\nThe jackdaw collects and decorates its nest with bright objects. Owing to its fondness for picking up coins, Linnaeus gave it the binomial name Corvus monedula, mǒnēdŭla being derived from the Latin stem of \"money\".\nThe collective noun for a group is a \"clattering\" of jackdaws. The jackdaw call is a familiar hard 'tchack' from which it gets its name. They are gregarious and vocal birds and live in small groups with complex social structures. It will commonly nest in chimneys, buildings, rock crevices and tree holes.")
-            .padding()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+        let note = Note(context: context)
+        note.text = "Jackdaw\nA fine grey and black bird with pale eyes, related to but smaller than the carrion crow.\nThough thievish and mischievous to the farmer, the jackdaw is a curious and intelligent bird. It has been observed using tools for complex problem solving, like its Corvid cousins.\nThe jackdaw collects and decorates its nest with bright objects. Owing to its fondness for picking up coins, Linnaeus gave it the binomial name Corvus monedula, mǒnēdŭla being derived from the Latin stem of \"money\".\nThe collective noun for a group is a \"clattering\" of jackdaws. The jackdaw call is a familiar hard 'tchack' from which it gets its name. They are gregarious and vocal birds and live in small groups with complex social structures. It will commonly nest in chimneys, buildings, rock crevices and tree holes."
+        return NoteView(note: note)
     }
 }
