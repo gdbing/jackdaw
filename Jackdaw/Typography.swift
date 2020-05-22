85e004876961ccc10d405878747ea70dea01ccde
Graham Bing <grrraham@gmail.com>
2020-05-22 13:52:30 -0700

    Type Formatting
    added a preview to Typography.swift

--- Jackdaw/Typography.swift
+++ Jackdaw/Typography.swift
@@ -12,76 +12,81 @@ class Typography {
     
     let styleColor = Color.init(red: 0.0, green: 0.1, blue: 0.5)
     let styleUIColor = UIColor.init(red: 0.0, green: 0.1, blue: 0.5, alpha: 1.0)
-    
-    static func previewStringFrom(string: String) -> NSAttributedString {
-        
-        // TODO remove multiple linebreaks
-        // TODO remove trailing linebreaks
+    let bodyUIFont = UIFont.init(name: "AvenirNext-Regular", size: 14.0)!
+    let titleUIFont = UIFont(descriptor: UIFont.init(name: "AvenirNext-DemiBold", size: 16.0)!.fontDescriptor.addingAttributes([
+        UIFontDescriptor.AttributeName.featureSettings: [
+            [
+                UIFontDescriptor.FeatureKey.featureIdentifier: kLowerCaseType,
+                UIFontDescriptor.FeatureKey.typeIdentifier: kLowerCaseSmallCapsSelector
+            ]
+        ]
+    ]), size: 16.0)
+
+    func previewStringFrom(string: String) -> NSAttributedString {
         
         if string.count < 1 { return NSAttributedString(string: "") }
-        // Body Typography
-        let bodyFont = UIFont.init(name: "AvenirNext-Regular", size: 14.0)!
-        let bodyAttributes = [NSAttributedString.Key.font: bodyFont]
-
-        // Title Typography
-        let systemFont = UIFont.init(name: "AvenirNext-DemiBold", size: 16.0)!
-        let smallCapsDesc = systemFont.fontDescriptor.addingAttributes([
-            UIFontDescriptor.AttributeName.featureSettings: [
-                [
-                    UIFontDescriptor.FeatureKey.featureIdentifier: kLowerCaseType,
-                    UIFontDescriptor.FeatureKey.typeIdentifier: kLowerCaseSmallCapsSelector
-                ]
-            ]
-        ])
-        let titleFont = UIFont(descriptor: smallCapsDesc, size: 16.0)
 
         guard let index = string.firstIndex(of: "\n") else {
             return NSAttributedString(string: string,
-                                      attributes: [NSAttributedString.Key.font : titleFont])
+                                      attributes: [NSAttributedString.Key.font : titleUIFont])
         }
 
         let title = String(string[..<index])
         let body = String(string[index...])
 
-        let titleBody = NSMutableAttributedString(string: title + body, attributes: bodyAttributes)
-        titleBody.addAttribute(NSAttributedString.Key.font, value: titleFont, range: NSRange(location: 0, length: title.count))
+        let titleBody = NSMutableAttributedString(string: title + body, attributes: [NSAttributedString.Key.font: self.bodyUIFont])
+        titleBody.addAttribute(NSAttributedString.Key.font, value: titleUIFont, range: NSRange(location: 0, length: title.count))
         
         return titleBody
     }
     
-    static func attributedStringFrom(string: String) -> NSAttributedString {
+    func attributedStringFrom(string: String) -> NSAttributedString {
         if string.count < 1 { return NSAttributedString(string: "") }
-        
-        // Body Typography
-        let bodyFont = UIFont.init(name: "AvenirNext-Regular", size: 14.0)!//UIFont.systemFont(ofSize: 14.0, weight: .regular)
-        let lineSpacing = NSMutableParagraphStyle()
-        lineSpacing.paragraphSpacing = 4
-        let bodyAttributes = [NSAttributedString.Key.font: bodyFont,
-                              NSAttributedString.Key.paragraphStyle: lineSpacing]
-        
-        // Title Typography
-        let systemFont = UIFont.init(name: "AvenirNext-DemiBold", size: 16.0)!//UIFont.systemFont(ofSize: 14.0, weight: .bold)
-        let smallCapsDesc = systemFont.fontDescriptor.addingAttributes([
-            UIFontDescriptor.AttributeName.featureSettings: [
-                [
-                    UIFontDescriptor.FeatureKey.featureIdentifier: kLowerCaseType,
-                    UIFontDescriptor.FeatureKey.typeIdentifier: kLowerCaseSmallCapsSelector
-                ]
-            ]
-        ])
-        let titleFont = UIFont(descriptor: smallCapsDesc, size: 16.0)
         
         guard let index = string.firstIndex(of: "\n") else {
             return NSAttributedString(string: string,//.lowercased(with: .current),
-                                      attributes: [NSAttributedString.Key.font : titleFont])
+                                      attributes: [NSAttributedString.Key.font : titleUIFont])
         }
         
         let title = String(string[..<index])//.lowercased(with: .current)
         let body = String(string[index...])
         
+        let lineSpacing = NSMutableParagraphStyle()
+        lineSpacing.paragraphSpacing = 4
+        let bodyAttributes = [NSAttributedString.Key.font: bodyUIFont,
+                              NSAttributedString.Key.paragraphStyle: lineSpacing]
+        
         let titleBody = NSMutableAttributedString(string: title + body, attributes: bodyAttributes)
-        titleBody.addAttribute(NSAttributedString.Key.font, value: titleFont, range: NSRange(location: 0, length: title.count))
+        titleBody.addAttribute(NSAttributedString.Key.font, value: titleUIFont, range: NSRange(location: 0, length: title.count))
         
         return titleBody
+    }
+}
+
+// MARK: - preview
+
+struct PreviewLabel: UIViewRepresentable {
+    let text: String
+    func makeUIView(context: Context) -> UILabel {
+        let label = UILabel()
+        label.preferredMaxLayoutWidth = 1.0
+        label.numberOfLines = 20
+        label.attributedText = Typography().attributedStringFrom(string: self.text)
+        label.sizeToFit()
+        return label
+    }
+    
+    func updateUIView(_ uiView: UILabel, context: Context) {
+        
+    }
+}
+
+struct TypographyPreview: PreviewProvider {
+    static var previews: some View {
+
+        PreviewLabel(text: "Months before I played Dark Souls I came across a long list of the things players should finish up at the end of their first playthrough.\nFight optional bosses, farm humanity, collect and upgrade weapons, kindle bonfires. Trade for useful items, level up covenants. And last of all, quite simply: kill everyone.")
+            .padding()
+            .previewLayout(.fixed(width: 350, height: 200))
+
     }
 }
